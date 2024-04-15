86da06d094e4828df7a908db97c7517c3d61f17b
Stas Tarasenko <fieryybird@gmail.com>
2024-04-15 12:30:27 +0000

    build docker image with params

--- vars/buildDockerImage.groovy
+++ vars/buildDockerImage.groovy
@@ -1,10 +1,10 @@
 #!/usr/bin/env groovy
 
-def call() {
+def call(String imageName) {
     echo "building the docker image..."
         withCredentials([usernamePassword(credentialsId: 'nexus-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-            sh 'docker build -t 37.187.124.95:8888/demojavapp:v2.0 .'
+            sh "docker build -t $imageName ."
             sh "echo $PASS | docker login 37.187.124.95:8888 -u $USER --password-stdin"
-            sh 'docker push 37.187.124.95:8888/demojavapp:v2.0'
+            sh "docker push $imageName"
     }
 }
